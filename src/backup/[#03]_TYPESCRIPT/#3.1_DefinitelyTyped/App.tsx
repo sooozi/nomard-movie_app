def4d0b235c49651cbce12aaf12050cd50d26414
kimsuji <skme1103@naver.com>
2023-10-25 22:03:34 +0900

    231025_마스터클래스(타입스크립트)_3.1_end

---
+++ src/backup/[#03]_TYPESCRIPT/#3.1_DefinitelyTyped/App.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import React from "react";
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100px;
+  height: 100px;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+
+
+function App() {
+  return ( 
+    <Wrapper>
+      <Title>hello</Title>
+    </Wrapper>
+  );
+}
+
+export default App;
+
---
+++ src/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import App from './App';
+
+//const root = ReactDOM.createRoot(document.getElementById('root')); => 오류발생!!!!!!!
+//https://velog.io/@syncstar/HTMLElement-null-%ED%98%95%EC%8B%9D%EC%9D%98-%EC%9D%B8%EC%88%98%EB%8A%94-Element-DocumentFragment-%ED%98%95%EC%8B%9D%EC%9D%98-%EB%A7%A4%EA%B0%9C-%EB%B3%80%EC%88%98%EC%97%90-%ED%95%A0%EB%8B%B9%EB%90%A0-%EC%88%98-%EC%97%86%EC%8A%B5%EB%8B%88%EB%8B%A4.-%EC%98%A4%EB%A5%98
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement   // 여기 HTMLElement 넣어주면 끝
+);
+
+const darkTheme = {
+  textColor:"whitesmoke",
+  backgroundColor: "#111"
+};
+
+const lightTheme = {
+  textColor:"#111",
+  backgroundColor: "whitesmoke"
+};
+
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
+
