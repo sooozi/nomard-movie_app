729784cb24b7c7392cc52e7edc73bc560b871cd6
kimsuji <skme1103@naver.com>
2023-10-31 22:57:51 +0900

    231031_마스터클래스_5.8_end

--- backup/[#5]_CRYPTO TRACKER/#5.8_Nested Routes part Two/src/routes/Coin.tsx
+++ backup/[#5]_CRYPTO TRACKER/#5.8_Nested Routes part Two/src/routes/Coin.tsx
@@ -1,15 +1,4 @@
-import { useEffect, useState } from "react";
-import {
-    Link,
-    Route,
-    Switch,
-    useLocation,
-    useParams,
-    useRouteMatch,
-} from "react-router-dom";
 import styled from "styled-components";
-import Chart from "./Chart";
-import Price from "./Price";
 
 const Title = styled.h1`
   font-size: 48px;
@@ -135,84 +124,84 @@ interface PriceData {
     };
 }
 
-function Coin() {
-  const [loading, setLoading] = useState(true);
-  const { coinId } = useParams<RouteParams>();
-  const { state } = useLocation<RouteState>();
-  const [info, setInfo] = useState<InfoData>();
-  const [priceInfo, setPriceInfo] = useState<PriceData>();
-  const priceMatch = useRouteMatch("/:coinId/price");
-  const chartMatch = useRouteMatch("/:coinId/chart");
-  useEffect(() => {
-    (async() => {
-        const infoData = await (
-            await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
-        ).json();
-        const priceData = await (
-            await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
-        ).json();
-        setInfo(infoData);
-        setPriceInfo(priceData);
-        setLoading(false);
-    })();
-}, [coinId]);
-  return (
-    <Container>
-      <Header>
-        <Title>
-            {state?.name ? state.name : loading ? "Loading..." : info?.name}
-        </Title>
-      </Header>
-      {loading ? (
-        <Loader>Loading...</Loader>
-      ) : (
-        <>
-          <Overview>
-            <OverviewItem>
-              <span>Rank:</span>
-              <span>{info?.rank}</span>
-            </OverviewItem>
-            <OverviewItem>
-              <span>Symbol:</span>
-              <span>${info?.symbol}</span>
-            </OverviewItem>
-            <OverviewItem>
-              <span>Open Source:</span>
-              <span>{info?.open_source ? "Yes" : "No"}</span>
-            </OverviewItem>
-          </Overview>
-          <Description>{info?.description}</Description>
-          <Overview>
-            <OverviewItem>
-              <span>Total Suply:</span>
-              <span>{priceInfo?.total_supply}</span>
-            </OverviewItem>
-            <OverviewItem>
-              <span>Max Supply:</span>
-              <span>{priceInfo?.max_supply}</span>
-            </OverviewItem>
-          </Overview>
+// function Coin() {
+//   const [loading, setLoading] = useState(true);
+//   const { coinId } = useParams<RouteParams>();
+//   const { state } = useLocation<RouteState>();
+//   const [info, setInfo] = useState<InfoData>();
+//   const [priceInfo, setPriceInfo] = useState<PriceData>();
+//   const priceMatch = useRouteMatch("/:coinId/price");
+//   const chartMatch = useRouteMatch("/:coinId/chart");
+//   useEffect(() => {
+//     (async() => {
+//         const infoData = await (
+//             await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+//         ).json();
+//         const priceData = await (
+//             await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+//         ).json();
+//         setInfo(infoData);
+//         setPriceInfo(priceData);
+//         setLoading(false);
+//     })();
+// }, [coinId]);
+//   return (
+//     <Container>
+//       <Header>
+//         <Title>
+//             {state?.name ? state.name : loading ? "Loading..." : info?.name}
+//         </Title>
+//       </Header>
+//       {loading ? (
+//         <Loader>Loading...</Loader>
+//       ) : (
+//         <>
+//           <Overview>
+//             <OverviewItem>
+//               <span>Rank:</span>
+//               <span>{info?.rank}</span>
+//             </OverviewItem>
+//             <OverviewItem>
+//               <span>Symbol:</span>
+//               <span>${info?.symbol}</span>
+//             </OverviewItem>
+//             <OverviewItem>
+//               <span>Open Source:</span>
+//               <span>{info?.open_source ? "Yes" : "No"}</span>
+//             </OverviewItem>
+//           </Overview>
+//           <Description>{info?.description}</Description>
+//           <Overview>
+//             <OverviewItem>
+//               <span>Total Suply:</span>
+//               <span>{priceInfo?.total_supply}</span>
+//             </OverviewItem>
+//             <OverviewItem>
+//               <span>Max Supply:</span>
+//               <span>{priceInfo?.max_supply}</span>
+//             </OverviewItem>
+//           </Overview>
 
-          <Tabs>
-            <Tab isActive={chartMatch !== null}>
-              <Link to={`/${coinId}/chart`}>Chart</Link>
-            </Tab>
-            <Tab isActive={priceMatch !== null}>
-              <Link to={`/${coinId}/price`}>Price</Link>
-            </Tab>
-          </Tabs>
+//           <Tabs>
+//             <Tab isActive={chartMatch !== null}>
+//               <Link to={`/${coinId}/chart`}>Chart</Link>
+//             </Tab>
+//             <Tab isActive={priceMatch !== null}>
+//               <Link to={`/${coinId}/price`}>Price</Link>
+//             </Tab>
+//           </Tabs>
 
-          <Switch>
-            <Route path={`/:coinId/price`}>
-              <Price />
-            </Route>
-            <Route path={`/:coinId/chart`}>
-              <Chart />
-            </Route>
-          </Switch>
-        </>
-      )}
-    </Container>
-  );
-}
-export default Coin;
+//           <Switch>
+//             <Route path={`/:coinId/price`}>
+//               <Price />
+//             </Route>
+//             <Route path={`/:coinId/chart`}>
+//               <Chart />
+//             </Route>
+//           </Switch>
+//         </>
+//       )}
+//     </Container>
+//   );
+// }
+// export default Coin;
