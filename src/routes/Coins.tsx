d67dc6ff97a7b3ccfb3919c4238fe3a79771b4c4
kimsuji <skme1103@naver.com>
2023-11-01 21:48:39 +0900

    231101_마스터클래스_5.9_end

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { fetchCoins } from "../api";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -55,23 +56,14 @@ interface CoinInterface {
 }
 
 function Coins() {
-    const [coins, setCoins] = useState<CoinInterface[]>([]);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        (async() => {
-            const response = await fetch("https://api.coinpaprika.com/v1/coins");
-            const json = await response.json();
-            setCoins(json.slice(0, 100));
-            setLoading(false);
-        })()
-    }, []);
+    const { isLoading, data } = useQuery<CoinInterface[]>("allCoins", fetchCoins)
   return (
     <Container>
       <Header>
         <Title>코인</Title>
       </Header>
-      {loading ? ("Loading...🪄") : (<CoinsList>
-        {coins.map((coin) => (
+      {isLoading ? ("Loading...🪄") : (<CoinsList>
+        {data?.slice(0, 100).map((coin) => (
           <Coin key={coin.id}>
             <Link to={{
                 pathname: `/${coin.id}`,
